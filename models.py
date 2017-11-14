913ed86cfdd63e3f0fabd99b5898bf4dbf9f2024
frank <i@nyan.im>
2017-11-14 20:22:15 +0800

    Add agent name field

--- models.py
+++ models.py
@@ -8,14 +8,16 @@ class Service(Base):
     id = Column(Integer, primary_key=True)
     server_name = Column(String(50))
     name = Column(String(50))
+    agent = Column(String(50))
     url = Column(String(50))
     http = Column(Integer) # 0=off, 1=on, -1=disable
-    https = Column(Integer) 
+    https = Column(Integer)
     time = Column(DateTime(timezone=True), server_default=func.now())
 
-    def __init__(self, server_name, name, url, http, https):
+    def __init__(self, server_name, name, agent, url, http, https):
         self.server_name = server_name
         self.name = name
+        self.agent = agent
         self.url = url
         self.http = http
         self.https = https
@@ -27,14 +29,16 @@ class Server(Base):
     __tablename__ = 'servers'
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
+    agent = Column(String(50))
     server = Column(String(50))
     status = Column(Integer)#0=off,1=on,2=unstable
     ping = Column(Float)
     time = Column(DateTime(timezone=True), server_default=func.now())
 
-    def __init__(self, name, server, status, ping):
+    def __init__(self, name, server, agent, status, ping):
         self.server = server
         self.name = name
+        self.agent = agent
         self.status = status
         self.ping = ping
 
