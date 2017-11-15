1c997ce6819749f14370d972a73b9e03260f744a
frank <i@nyan.im>
2017-11-15 20:27:30 +0800

    Remove

--- monitor/__init__.py
+++ monitor/__init__.py
@@ -5,10 +5,6 @@ from .controllers.AgentAPIController import agent_api
 from .controllers.WebController import web_module
 
 app = Flask(__name__, static_url_path='/static')  # 定义/static目录为静态文件目录
-#app.config.from_pyfile('../config.py')
-
-#config = json.loads(open("master_config.json").read())
-
 
 app.register_blueprint(agent_api)
 app.register_blueprint(web_module)
